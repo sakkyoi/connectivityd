b69d841ce6f4e290ff47946412bedb7e97c7d760
Weichung Cheng <22865542+sakkyoi@users.noreply.github.com>
2026-03-31 17:37:49 +0800

    make some private mod being public

--- crates/connectivity-dbus/src/lib.rs
+++ crates/connectivity-dbus/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod error;
 pub mod network;
 pub mod server;
-mod ethernet;
-mod vpn;
-mod bluetooth;
-mod advertising;
-mod gatt_host;
+pub mod ethernet;
+pub mod vpn;
+pub mod bluetooth;
+pub mod advertising;
+pub mod gatt_host;
 
 pub use server::DbusServer;
